fe099e6ecbf98a3204066c0506eea1091f35109a
logicp <adventurist@gmail.com>
2020-08-18 20:37:57 -0400

    updating build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-g++ -std=c++17 -I include src/main.cpp src/open_space.cpp -o open_space
+g++ -std=c++17 -pthread -I include src/main.cpp src/open_space.cpp src/node.cpp -o open_space
